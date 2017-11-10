f3634cc5e11380fcf3d6005df22f70647ebd39b0
XinlingQiu <2042912491@qq.com>
2017-11-10 12:42:33 +0800

    add test file

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+
+
+  get 'welcome/index'
+
+  root "welcome#index"
+
+  get 'diary/index'
+
+root "diary#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
